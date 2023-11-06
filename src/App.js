5cd83d23fc9916d5d178a9a504e14d53be42731c
Syrymbek Zhumazhan <75174477+srmbackisdeveloper@users.noreply.github.com>
2023-11-07 02:25:29 +0600

    minor changes 1

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
 import './App.css';
+const imageSrc = 'https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/09/sushi-sashimi-1296x728-header.jpg?w=1155&h=1528'
 
 function App() {
-  const imageSrc = 'https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/09/sushi-sashimi-1296x728-header.jpg?w=1155&h=1528'
+  useEffect(() => {
+    const buyButton = document.getElementById("buy");
+    const orderButton = document.getElementById("order");
 
-  let tg = window.Telegram.WebApp;
-  let buy = document.getElementById("buy")
-  let order = document.getElementById("order")
+    buyButton.addEventListener("click", () => {
+      document.getElementById("main").style.display = "none";
+      document.getElementById("form-info").style.display = "block";
 
-  buy.addEventListener("click", () => {
-    document.getElementById("main").style.display = "none";
-    document.getElementById("form-info").style.display = "block";
+      // You may set the value for user_name-info here (assuming it's an input)
+      document.getElementById("user_name-info").value = tg.initDataUnsafe.user.first_name + " " + tg.initDataUnsafe.user.last_name;
+    });
 
-    document.getElementById("user_name-info").value
-    = tg.initDataUnsafe.user.first_name + " " + tg.initDataUnsafe.user.last_name
-  })
+    orderButton.addEventListener("click", () => {
+      tg.close();
+    });
 
-  order.addEventListener("click", () => {
-    tg.close();
-  })
+    return () => {
+      // Cleanup event listeners when the component unmounts
+      buyButton.removeEventListener("click");
+      orderButton.removeEventListener("click");
+    };
+  }, []);
 
 
   return (
-    <div className="container">
-      <div className='main'>
+     <div className="container">
+      <div id='main' className='main'>
         <h1>Sbake Sushi</h1>
         <p>Sushi Set {"<Syrymbek>"}</p>
-        <img className='main-image' src={imageSrc} />
+        <img className='main-image' src={imageSrc} alt="Sushi Set" />
         <button id='buy'>Buy</button>
       </div>
 
-      <form id='form-info'>
-        <input type='text' placeholder='Name' id='user_name' />
+      <form id='form-info' style={{ display: 'none' }}>
+        <input type='text' placeholder='Name' id='user_name-info' />
         <input type='email' placeholder='Email' id='user_email' />
         <input type='tel' placeholder='Phone number' id='user_phone' />
         <button id='order'>Send Order</button>
       </form>
 
-      <button id="buy">Order</button>
       <script src="https://telegram.org/js/telegram-web-app.js"></script>
     </div>
   );
